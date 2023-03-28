e99b4a6659a0e0ad79f824f152a9c62d0a0dfc29
ysj0226 <ysj@kanaries.org>
2023-03-28 17:53:44 +0800

    test_streamlit

--- tests/test_streamlit.py
+++ tests/test_streamlit.py
@@ -12,6 +12,15 @@ import pygwalker as pyg
 HERE = os.path.dirname(__file__)
 
 df = pd.read_csv(os.path.join(HERE, "bike_sharing_dc.csv"), parse_dates=['date'])
-df
-pyg.walk(df, env='Streamlit')
+# df
+# pyg.walk(df, env='Streamlit')
 
+html = pyg.to_html(df)
+html += """
+<script>
+window.addEventListener("load", function() {
+    window.alert("Hello Selenium!");
+  });
+</script>
+"""
+pyg.gwalker.display_html(html, env='Streamlit')
